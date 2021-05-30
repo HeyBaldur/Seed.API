67d73b751ce2b767cf16a415c2b37ab0f3421c97
gitrodo <fillocj.rc@gmail.com>
2021-05-30 17:15:58 -0600

    Update validation with FilterComparer

--- Seed.Interfaces/V1/ISolution.cs
+++ Seed.Interfaces/V1/ISolution.cs
@@ -1,4 +1,6 @@
 ﻿using Seed.Models.V1.Models;
+using Seed.Services.ErrorHandling;
+using Seed.Services.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,7 +12,7 @@ namespace Seed.Interfaces.V1
     {
         Task<DimSolution> Create(DimSolution solution);
         Task<IEnumerable<DimSolution>> GetAll();
-        Task<DimSolution> Update(DimSolution solution);
+        Task<ContentHandler<string>> Update(DimSolution solution);
         Task<DimSolutionFilter> CreateFilter(DimSolutionFilter filter);
     }
 }
